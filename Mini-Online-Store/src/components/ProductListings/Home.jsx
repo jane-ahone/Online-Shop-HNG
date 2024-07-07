0799e0d8da31f63437d1d5b5532b1dad55f34091
Jane Ahone <jane.eloundou@gmail.com>
2024-07-07 19:54:40 +0100

    Commit

--- Mini-Online-Store/src/components/ProductListings/Home.jsx
+++ Mini-Online-Store/src/components/ProductListings/Home.jsx
@@ -11,88 +11,92 @@ import product7 from "../../assets/images/sunscreen-page/Frame 12-6.svg";
 import product8 from "../../assets/images/sunscreen-page/Frame 12-7.svg";
 import product9 from "../../assets/images/sunscreen-page/Frame 12-8.svg";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="productlistings-home-main">
       <Header />
-
-      <div className="productlisting-content">
-        <div className="productlistings-header">
-          <p className="productlistings-title">All Products</p>
-          <img src={catIcon} alt="icon" className="cat-icon" />
-        </div>
-        <div className="product1">
-          <img src={product2} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product2">
-          <img src={product2} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product3">
-          <img src={product3} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product4">
-          <img src={product4} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product5">
-          <img src={product5} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product6">
-          <img src={product6} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product7">
-          <img src={product7} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product8">
-          <img src={product8} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product9">
-          <img src={product9} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product9">
-          <img src={product9} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product9">
-          <img src={product9} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
-        </div>
-        <div className="product9">
-          <img src={product9} />
-          <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
-          <p>13,000 sold</p>
-          <p className="prodPrice">$75</p>
+      <div className="productlisting-content-main">
+        <div className="productlisting-content">
+          <div className="productlistings-header">
+            <p className="productlistings-title">All Products</p>
+            <Link to="/sunscreen">
+              <img src={catIcon} alt="icon" className="cat-icon" />
+            </Link>
+          </div>
+          <div className="product1">
+            <img src={product2} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product2">
+            <img src={product2} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product3">
+            <img src={product3} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product4">
+            <img src={product4} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product5">
+            <img src={product5} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product6">
+            <img src={product6} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product7">
+            <img src={product7} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product8">
+            <img src={product8} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product9">
+            <img src={product9} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product9">
+            <img src={product9} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product9">
+            <img src={product9} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
+          <div className="product9">
+            <img src={product9} />
+            <p className="prodDesc">Banana Boat Light As Air SPF 50+</p>
+            <p>13,000 sold</p>
+            <p className="prodPrice">$75</p>
+          </div>
         </div>
       </div>
     </div>
