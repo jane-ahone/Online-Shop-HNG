3b5d2b0d13a472e4565c2d0bb6a920b714de0be8
Jane Ahone <jane.eloundou@gmail.com>
2024-07-08 06:28:45 +0100

    Added checkout page content

--- Mini-Online-Store/src/components/Checkout/Checkout.jsx
+++ Mini-Online-Store/src/components/Checkout/Checkout.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import "./Checkout.css";
+import Header from "../CategoryProducts/Header";
+import arrowRightDouble from "../../assets/icons/arrow-right-double.svg";
+import Cart from "../CategoryProducts/Cart/Cart";
 
 const Checkout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <div className="checkout-main">
+        <div className="checkout-main-header">
+          <p>
+            Product
+            <img
+              src={arrowRightDouble}
+              alt="right-arrow-icon"
+              className="arrow-right-double"
+            />
+          </p>
+          <p>
+            My Cart
+            <img
+              src={arrowRightDouble}
+              alt="right-arrow-icon"
+              className="arrow-right-double"
+            />
+          </p>
+          <p>
+            Checkout
+            <img
+              src={arrowRightDouble}
+              alt="right-arrow-icon"
+              className="arrow-right-double"
+            />
+          </p>
+          <p>
+            Order Complete
+            <img
+              src={arrowRightDouble}
+              alt="right-arrow-icon"
+              className="arrow-right-double"
+            />
+          </p>
+        </div>
+        <div className="checkout-main-contents">
+          <Cart />
+          <div className="shipping-information"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
