83888e38d4b86eba83efea82bb16fb26629389e3
Jane Ahone <jane.eloundou@gmail.com>
2024-07-13 17:41:29 +0100

    God abeg

--- Mini-Online-Store/src/components/CategoryProducts/Global/CircularProgress/CircularIndeterminate.jsx
+++ Mini-Online-Store/src/components/CategoryProducts/Global/CircularProgress/CircularIndeterminate.jsx
@@ -4,7 +4,18 @@ import Box from "@mui/material/Box";
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        display: "flex",
+        position: "fixed",
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        zIndex: "30",
+        backgroundColor: "#fafafa",
+      }}
+    >
       <CircularProgress />
     </Box>
   );
